ab55f7568c326394d9d57a6949556a81c1a8b585
Curia <admin@curiadesign.net>
2020-12-29 16:08:16 +1100

    Location set button

--- components/Weather/WeatherCurrent.tsx
+++ components/Weather/WeatherCurrent.tsx
@@ -5,43 +5,66 @@ interface geolocationInterface {
   lat: number;
   long: number;
   error?: string | GeolocationPositionError;
+  isSet: boolean;
 }
 
 const WeatherCurrent: React.FC = () => {
   const [geolocation, setGeolocation] = useState<geolocationInterface>({
     lat: 0,
     long: 0,
+    isSet: false,
   });
 
-  useEffect(() => {
+  const setLocation = () => {
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setGeolocation({
-          lat: position.coords.latitude,
-          long: position.coords.longitude,
-        });
-      }, (error) => {
-        console.log(error)
-        setGeolocation({
-          ...geolocation,
-          error: error,
-        });
-      });
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setGeolocation({
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+            isSet: true,
+            error: null,
+          });
+        },
+        (error) => {
+          console.log(error);
+          setGeolocation({
+            ...geolocation,
+            error: error,
+          });
+        }
+      );
     } else {
       setGeolocation({
         ...geolocation,
         error: "No geolocation avaliable",
       });
     }
-  }, []);
+  };
 
-  return !geolocation.error ? (
-    <div className="text-white h-full justify-end  flex flex-col">
-      <p className="text-7xl">{geolocation.lat}</p>
-      <p className="text-4xl">{geolocation.long}</p>
+  useEffect(() => {}, []);
+
+  return (
+    <div className="text-white h-full justify-end flex flex-col">
+      {!geolocation.isSet ? (
+        <div>
+          <button
+            className="ml-3 p-1 bg-blue-400 rounded"
+            onClick={() => setLocation()}
+          >
+            Request Location
+          </button>
+          <button className="ml-3 p-1 bg-blue-400 rounded">
+            Search Location
+          </button>
+        </div>
+      ) : (
+        <>
+          <p className="text-7xl">{geolocation.lat}</p>
+          <p className="text-4xl">{geolocation.long}</p>
+        </>
+      )}
     </div>
-  ) : (
-    <p>No geolocation, sorry.</p>
   );
 };
 
